654f7af0823514333bad47d2e1dbdfca98dad6b9
Lars Kruse <lakr@thetechcollective.eu>
2025-10-05 13:21:38 +0200

    ready to run from codespace

--- .devcontainer/denmark/postCreateCommand.sh
+++ .devcontainer/denmark/postCreateCommand.sh
@@ -9,6 +9,8 @@ echo "$PREFIX Running $(basename $0)"
 echo "$PREFIX Setting up git configuration to support .gitconfig in devcontainer root"
 git config --local --get include.path | grep -e ../.devcontainer/denmark/.gitconfig >/dev/null 2>&1 || git config --local --add include.path ../.devcontainer/denmark/.gitconfig
 
+# echo "$PREFIX Setting up sparse checkout"
+# ln -sf /workspaces/devopsdays-web/.devcontainer/denmark/sparse-checkout .git/info/sparse-checkout
 
 echo "$PREFIX SUCCESS"
 exit 0
