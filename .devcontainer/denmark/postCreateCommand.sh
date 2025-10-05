eb43669280a28cdbafc8e4a37c84c4808ef49eb3
Lars Kruse <lakruzz@inc-inc.dk>
2025-10-05 12:09:19 +0000

    updated container

--- .devcontainer/denmark/postCreateCommand.sh
+++ .devcontainer/denmark/postCreateCommand.sh
@@ -9,6 +9,9 @@ echo "$PREFIX Running $(basename $0)"
 echo "$PREFIX Setting up git configuration to support .gitconfig in devcontainer root"
 git config --local --get include.path | grep -e ../.devcontainer/denmark/.gitconfig >/dev/null 2>&1 || git config --local --add include.path ../.devcontainer/denmark/.gitconfig
 
+echo "$PREFIX Setting up GitHub CLI to use devopsdays-dk/devopsdays-web as default repo"
+gh repo set-default devopsdays-dk/devopsdays-web
+
 # echo "$PREFIX Setting up sparse checkout"
 # ln -sf /workspaces/devopsdays-web/.devcontainer/denmark/sparse-checkout .git/info/sparse-checkout
 
